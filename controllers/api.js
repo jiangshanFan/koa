f2335f3b1412d737825c082ea8793031c5fc8f07
jiangshan.fan <745989449@qq.com>
2020-01-07 17:33:11 +0800

    excel.js & simple excel

--- controllers/api.js
+++ controllers/api.js
@@ -6,6 +6,12 @@ const products = require('../products');
 
 const APIError = require('../rest').APIError;
 
+// excel
+const path = require('path');
+const fs = require('mz/fs');
+const saved_dir = path.normalize(__dirname + path.sep + '..' + path.sep + 'saved-docs');
+console.log(`documents will be saved in ${saved_dir}`);
+
 /**Ä£ÄâTODOÊý¾Ý¿â */
 var gid = 0;
 
@@ -124,7 +130,41 @@ module.exports = {
       throw new APIError('notfound', 'Todo not found by id: ' + ctx.params.id);
     }
     ctx.rest(todos.splice(index, 1)[0]);
-  }
+  },
+
+
+/** Excel */
+  'GET /api/sheets/:id': async (ctx, next) => {
+    var s, fp = path.join(saved_dir, '.' + ctx.params.id);
+    console.log(`load from file ${fp}...`);
+    s = await fs.readFile(fp, 'utf8');
+    ctx.rest(JSON.parse(s));
+  },
+  'PUT /api/sheets/:id': async (ctx, next) => {
+    var
+      fp = path.join(saved_dir, '.' + ctx.params.id),
+      title = ctx.request.body.title,
+      rows = ctx.request.body.rows,
+      data;
+    if (!title) {
+      throw new APIError('invalid_data', 'invalid title');
+    }
+    if (!Array.isArray(rows)) {
+      throw new APIError('invalid_data', 'invalid rows');
+    }
+    data = {
+      title: title,
+      rows: rows
+    };
+    await fs.writeFile(fp, JSON.stringify({
+      title: title,
+      rows: rows
+    }), 'utf8');
+    console.log(`wrote to file ${fp}.`);
+    ctx.rest({
+      id: ctx.params.id
+    });
+  },
 };
 
 /**
