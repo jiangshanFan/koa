57a695148e2edf0d110e5cdfc3a6eae08f16de16
jiangshan.fan <745989449@qq.com>
2020-01-07 14:43:16 +0800

    MVVM & REST API application(TODO)

--- controllers/api.js
+++ controllers/api.js
@@ -6,6 +6,37 @@ const products = require('../products');
 
 const APIError = require('../rest').APIError;
 
+/**Ä£ÄâTODOÊý¾Ý¿â */
+var gid = 0;
+
+function nextId() {
+  gid++;
+  return 't' + gid;
+}
+
+var todos = [
+  {
+    id: nextId(),
+    name: 'Learn Git',
+    description: 'Learn how to use git as distributed version control'
+  },
+  {
+    id: nextId(),
+    name: 'Learn JavaScript',
+    description: 'Learn JavaScript, Node.js, NPM and other libraries'
+  },
+  {
+    id: nextId(),
+    name: 'Learn Python',
+    description: 'Learn Python, WSGI, asyncio and NumPy'
+  },
+  {
+    id: nextId(),
+    name: 'Learn Java',
+    description: 'Learn Java, Servlet, Maven and Spring'
+  }
+];
+
 module.exports = {
   'GET /api/products': async (ctx, next) => {
     ctx.rest({
@@ -26,6 +57,73 @@ module.exports = {
     } else {
       throw new APIError('product: not_found', 'product not found by id.');
     }
+  },
+
+
+/** TODO */
+  'GET /api/todos': async (ctx, next) => {
+    ctx.rest({
+      todos: todos
+    });
+  },
+
+  'POST /api/todos': async (ctx, next) => {
+    var
+      t = ctx.request.body,
+      todo;
+    if (!t.name || !t.name.trim()) {
+      throw new APIError('invalid_input', 'Missing name');
+    }
+    if (!t.description || !t.description.trim()) {
+      throw new APIError('invalid_input', 'Missing description');
+    }
+    todo = {
+      id: nextId(),
+      name: t.name.trim(),
+      description: t.description.trim()
+    };
+    todos.push(todo);
+    ctx.rest(todo);
+  },
+
+  'PUT /api/todos/:id': async (ctx, next) => {
+    var
+      t = ctx.request.body,
+      index = -1,
+      i, todo;
+    if (!t.name || !t.name.trim()) {
+      throw new APIError('invalid_input', 'Missing name');
+    }
+    if (!t.description || !t.description.trim()) {
+      throw new APIError('invalid_input', 'Missing description');
+    }
+    for (i = 0; i < todos.length; i++) {
+      if (todos[i].id === ctx.params.id) {
+        index = i;
+        break;
+      }
+    }
+    if (index === -1) {
+      throw new APIError('notfound', 'Todo not found by id: ' + ctx.params.id);
+    }
+    todo = todos[index];
+    todo.name = t.name.trim();
+    todo.description = t.description.trim();
+    ctx.rest(todo);
+  },
+
+  'DELETE /api/todos/:id': async (ctx, next) => {
+    var i, index = -1;
+    for (i = 0; i < todos.length; i++) {
+      if (todos[i].id === ctx.params.id) {
+        index = i;
+        break;
+      }
+    }
+    if (index === -1) {
+      throw new APIError('notfound', 'Todo not found by id: ' + ctx.params.id);
+    }
+    ctx.rest(todos.splice(index, 1)[0]);
   }
 };
 
